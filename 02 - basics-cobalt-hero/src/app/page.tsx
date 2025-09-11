5053697e422b854087fd9cf1dfece9d216572715
rohannrk <rohanrk39357@gmail.com>
2025-09-11 06:03:34 +0530

    Keep only basics-of-css project on basics-of-css branch

--- 02 - basics-cobalt-hero/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Hero } from "@/components/hero";
-
-export default function Home() {
-  return (
-    <div className="h-screen bg-neutral-900 flex justify-center">
-      <Hero/>
-    </div>
-  );
-}
