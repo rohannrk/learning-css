1ec2b5a81f315f9ecdea199729b0d43d9559671f
rohannrk <rohanrk39357@gmail.com>
2025-09-11 07:59:23 +0530

    Keep only part-4 project on part-4-custom-style-grid branch

--- 00 - basics-of-css/src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Container = ({children}: {children: React.ReactNode}) => {
-  return (
-    <div className="container">
-      {children}
-    </div>
-  );
-};
